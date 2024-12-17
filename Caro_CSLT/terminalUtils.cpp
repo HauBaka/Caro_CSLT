e5380a5cd537087f6bea3140d3db2ae0aa85893b
HauBaka <tuquynh7381@gmail.com>
2024-12-18 00:33:11 +0700

    thêm ghi chú

--- Caro_CSLT/terminalUtils.cpp
+++ Caro_CSLT/terminalUtils.cpp
@@ -1,14 +1,17 @@
 ﻿#include "terminalUtils.h"
-void FixConsoleWindow() {//khong thay doi kich thuoc cua so
+void FixConsoleWindow() {/*Cố định cửa sổ*/
+	/*Không cho thay đồi kích thước cửa sổ*/
 	HWND consoleWindow = GetConsoleWindow();
 	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
 	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
 	SetWindowLong(consoleWindow, GWL_STYLE, style);
-
+	/*Di chuyển cửa sổ đến góc trái trên và đặt kích thước là 1280x720 px*/
 	MoveWindow(consoleWindow, 0,0, 1280, 720, TRUE);//ngang 172, doc 42 ki tu
-	ShowScrollBar(GetConsoleWindow(), SB_VERT, 0);
+	/*Tắt thanh kéo*/
+	ShowScrollBar(GetConsoleWindow(), SB_VERT, 0);//TO DO: Not work
 }
-void ShowConsoleCursor(bool showFlag)// tat con tro chuot
+/*Bật/tắt con trỏ chuột nhấp nhấy*/
+void ShowConsoleCursor(bool showFlag)
 {
 	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO     cursorInfo;
@@ -16,12 +19,14 @@ void ShowConsoleCursor(bool showFlag)// tat con tro chuot
 	cursorInfo.bVisible = showFlag;
 	SetConsoleCursorInfo(out, &cursorInfo);
 }
+/*Đi đến tọa độ x,y trên cửa sổ*/
 void GotoXY(int x, int y) {
 	COORD coord;
 	coord.X = x;
 	coord.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+/*Đếm số kí tự của text ở kiểu wstring*/
 int sizeOfText(wstring text) {
 	int count = 0;
 	bool c = false;
@@ -38,9 +43,11 @@ int sizeOfText(wstring text) {
 	}
 	return count;
 }
+/*Xóa cả 1 dòng*/
 void clearLine(int line) {
 	RGBPrint(0, line, "                                                                                                                                                                             ", white_pink, white_pink);
 }
+/*Thiết lập trò chơi*/
 void SetUpWindow() {
 	FixConsoleWindow();
 	ShowConsoleCursor(false);
